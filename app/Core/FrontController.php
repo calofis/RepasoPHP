b9e255110cbd60dd6dfa463f9118a439fb135b1d
rgcenteno <rgcenteno@DAW2-100.dip.local>
2022-11-15 14:25:16 +0100

    Versión 1.0

---
+++ app/Core/FrontController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Com\Daw2\Core;
+
+use Steampixel\Route;
+
+class FrontController{
+    
+    static function main(){
+        Route::add('/', 
+                function(){
+                    $controlador = new \Com\Daw2\Controllers\InicioController();
+                    $controlador->index();
+                }
+                , 'get');                
+                
+        Route::pathNotFound(
+            function(){
+                $controller = new \Com\Daw2\Controllers\ErroresController();
+                $controller->error404();
+            }
+        );
+        
+        Route::methodNotAllowed(
+            function(){
+                $controller = new \Com\Daw2\Controllers\ErroresController();
+                $controller->error405();
+            }
+        );
+        Route::run();
+    }
+}
+
